b7af3e88a15205be6c2fddd30341b01a729c72f9
slackbot115 <viniccius.carvalho22@gmail.com>
2022-02-04 16:59:26 -0300

    Requisitos finalizados

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -2,7 +2,6 @@
 
 namespace Calculadora.ConsoleApp
 {
-
     internal class Program
     {
         static void Main(string[] args)
@@ -11,50 +10,67 @@ namespace Calculadora.ConsoleApp
 
             while (true)
             {
+                string[] historico = { };
                 Console.WriteLine("Digite a operacao que deseja realizar: ");
                 Console.WriteLine("1 - Soma\n2 - Subtracao\n3 - Multiplicacao\n4 - Divisao\n0 - Sair");
-                int op = int.Parse(Console.ReadLine());
-                if(op == 0)
+                try
                 {
-                    break;
-                }
+                    int op = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("Digite o primeiro numero: ");
-                double n1 = double.Parse(Console.ReadLine());
-                Console.WriteLine("Digite o segundo numero: ");
-                double n2 = double.Parse(Console.ReadLine());
-
-                if (op == 1)
-                {
-                    double res = n1 + n2;
-                    Console.WriteLine("Resultado da operacao: " + res);
-                }
-                else if (op == 2)
-                {
-                    double res = n1 - n2;
-                    Console.WriteLine("Resultado da operacao: " + res);
-                }
-                else if (op == 3)
-                {
-                    double res = n1 * n2;
-                    Console.WriteLine("Resultado da operacao: " + res);
-                }
-                else if (op == 4)
-                {
-                    if (n2 == 0)
+                    if (op == 0)
                     {
-                        Console.WriteLine("Impossivel dividir por 0!");
+                        Console.WriteLine("Fechando o programa...\n");
+                        Environment.Exit(0);
+                    }
+
+                    Console.WriteLine("Digite o primeiro numero: ");
+                    double n1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite o segundo numero: ");
+                    double n2 = double.Parse(Console.ReadLine());
+
+                    if (op == 1)
+                    {
+                        double res = n1 + n2;
+                        Console.WriteLine("Resultado da operacao: " + Math.Round(res, 2));
+                    }
+                    else if (op == 2)
+                    {
+                        double res = n1 - n2;
+                        Console.WriteLine("Resultado da operacao: " + Math.Round(res, 2));
+                    }
+                    else if (op == 3)
+                    {
+                        double res = n1 * n2;
+                        Console.WriteLine("Resultado da operacao: " + Math.Round(res, 2));
+                    }
+                    else if (op == 4)
+                    {
+                        if (n2 == 0)
+                        {
+                            Console.WriteLine("Impossivel dividir por 0!");
+                        }
+                        else
+                        {
+                            double res = n1 / n2;
+                            Console.WriteLine("Resultado da operacao: " + Math.Round(res, 2));
+                        }
+                    }
+                    else if (op == 0)
+                    {
+                        Console.WriteLine("Fechando o programa...\n");
+                        break;
                     }
                     else
                     {
-                        double res = n1 / n2;
-                        Console.WriteLine("Resultado da operacao: " + res);
+                        Console.WriteLine("Opcao invalida, digite novamente...");
                     }
                 }
-                else if(op == 0)
+                catch
                 {
-                    break;
+                    Console.WriteLine("Ocorreu um erro, digite novamente... ");
                 }
+
+                
             }
         }
     }
